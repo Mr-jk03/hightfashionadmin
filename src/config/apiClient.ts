494d16ca65b70609fb9530d41d8d9bcf8d4f54e3
unknown <vunh@digins.vn>
2025-03-11 09:36:54 +0700

    commit package

--- src/config/apiClient.ts
+++ src/config/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    //baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://server-a85p.onrender.com',
     headers:{
         'Content-Type':'application/json'
     }
